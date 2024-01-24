5277a0ff89b208cfa1e4a59e1bfc401b5a27c090
Prashant Sambhaji Mane <prashant.mane@in.techwave.net>
2024-01-24 20:25:06 +0530

    change in mongoose version use latest one

--- src/db/dbConnection.js
+++ src/db/dbConnection.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose");
 const db_url = "mongodb://127.0.0.1:27017/mydb";
 
 mongoose.connect(db_url, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
+    // useCreateIndex: true,
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+    // useFindAndModify: false
   })
   .then(() => {
     console.log("Connection Successful..!");
